e4de23b63cd01dc884b42c958aafe2778ea162ce
ChristopherRolando8 <christopher.rolando@fazzfinancial.com>
2025-02-21 16:15:52 +0700

    Fix image not showing

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
 	reactStrictMode: true,
 
 	images: {
+		// ? This ensures that only images from the official TMDB website can be optimized.
 		remotePatterns: [
 			{
 				protocol: 'https',
 				hostname: 'image.tmdb.org',
-				pathname: '/t/p/**',
+				pathname: '/**',
 			},
 		],
 	},
