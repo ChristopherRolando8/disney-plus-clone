c3ac6229a0a87b9e41c2e42fd4be4fb61333a90a
ChristopherRolando8 <christopher.rolando@fazzfinancial.com>
2025-02-21 16:11:12 +0700

    Fix image not showing

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
 	reactStrictMode: true,
 
 	images: {
-		domains: [
-			'www.your-domain-name.com',
-		]
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'image.tmdb.org',
+				pathname: '/t/p/**',
+			},
+		],
 	},
 };
 
